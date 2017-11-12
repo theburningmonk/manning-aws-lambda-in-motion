fe69d81ca9bcd031b233d83f0b838562dd210b00
theburningmonk <theburningmonk@gmail.com>
2017-11-12 21:20:18 +0000

    - unit 2.2

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,42 +1,28 @@
 'use strict';
 
-const co         = require('co');
-const Promise    = require('bluebird');
-const fs         = Promise.promisifyAll(require("fs"));
-const apiHandler = require('../lib/apiHandler');
-const http       = require('superagent-promise')(require('superagent'), Promise);
-const Mustache   = require('mustache');
+const co = require("co");
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
 
-const restaurantApiRoot = process.env.restaurant_api;
-
-const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 var html;
 
 function* loadHtml() {
   if (!html) {
-    console.log('loading index.html...');
-    html = fs.readFileAsync('static/index.html', 'utf-8');
-    console.log('loaded');
+    html = yield fs.readFileAsync('static/index.html', 'utf-8');
   }
-  
+
   return html;
 }
 
-function* getRestaurants() {
-  return (yield http.get(restaurantApiRoot)).body;
-}
-
-module.exports.handler = apiHandler(co.wrap(function* (event, context, callback) {
-  let template = yield loadHtml();
-  let restaurants = yield getRestaurants();
-  let dayOfWeek = days[ new Date().getDay() ];
-  let html = Mustache.render(template, { dayOfWeek, restaurants });
-
-  return {
+module.exports.handler = co.wrap(function* (event, context, callback) {
+  let html = yield loadHtml();
+  const response = {
     statusCode: 200,
+    body: html,
     headers: {
       'Content-Type': 'text/html; charset=UTF-8'
-    },
-    body: html
+    }
   };
-}));
+
+  callback(null, response);
+});
--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,29 +1,28 @@
 'use strict';
 
-const co         = require('co');
-const AWS        = require('aws-sdk');
-const dynamodb   = new AWS.DynamoDB.DocumentClient();
-const apiHandler = require('../lib/apiHandler');
+const co = require('co');
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const defaultResults = process.env.defaultResults || 8;
-const tableName      = process.env.restaurants_table;
+const tableName = process.env.restaurants_table;
 
 function* getRestaurants(count) {
   let req = {
-    TableName : tableName,
-    Limit : count
+    TableName: tableName,
+    Limit: count
   };
 
-  console.log(req);
   let resp = yield dynamodb.scan(req).promise();
   return resp.Items;
 }
 
-module.exports.handler = apiHandler(co.wrap(function* (event, context) {
+module.exports.handler = co.wrap(function* (event, context, cb) {
   let restaurants = yield getRestaurants(defaultResults);
-  
-  return {
+  let response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
-  };
-}));
+  }
+
+  cb(null, response);
+});
