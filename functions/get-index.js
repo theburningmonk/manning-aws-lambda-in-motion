89502f44ad6e8adb9ad37e5fc980b54d834cff78
theburningmonk <theburningmonk@gmail.com>
2017-11-12 22:50:57 +0000

    - unit 2.3

--- functions/get-index.js
+++ functions/get-index.js
@@ -3,6 +3,11 @@
 const co = require("co");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require('mustache');
+const http = require('superagent-promise')(require('superagent'), Promise);
+
+const restaurantsApiRoot = process.env.restaurants_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var html;
 
@@ -14,8 +19,16 @@ function* loadHtml() {
   return html;
 }
 
+function* getRestaurants() {
+  return (yield http.get(restaurantsApiRoot)).body;
+}
+
 module.exports.handler = co.wrap(function* (event, context, callback) {
-  let html = yield loadHtml();
+  let template = yield loadHtml();
+  let restaurants = yield getRestaurants();
+  let dayOfWeek = days[new Date().getDay()];
+  let html = Mustache.render(template, { dayOfWeek, restaurants });
+
   const response = {
     statusCode: 200,
     body: html,
