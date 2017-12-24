6ad0712bca21bec04a01f1263fd88f6697e8262e
theburningmonk <theburningmonk@gmail.com>
2017-12-24 03:26:25 +0000

    - added aws cred back to init step

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -2,6 +2,7 @@
 
 const co      = require('co');
 const Promise = require('bluebird');
+const awscred = Promise.promisifyAll(require('./../../functions/awscred'));
 
 let initialized = false;
 
@@ -16,6 +17,18 @@ let init = co.wrap(function* () {
   process.env.cognito_client_id = "test_cognito_client_id";
   process.env.cognito_user_pool_id = "us-east-1_DfuAwa0vB";
   process.env.cognito_server_client_id = "niv7esuaibla0tj5q36b6mvnr";
+
+  if (!process.env.AWS_ACCESS_KEY_ID) {
+    console.log("loading AWS credentials");
+
+    let cred = (yield awscred.loadAsync()).credentials;
+    
+    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+    if (cred.sessionToken) {
+      process.env.AWS_SESSION_TOKEN = cred.sessionToken;
+    }
+  }
 
   initialized = true;
 });
