0f8e409d158d193ac66e4ff3768f777d3619e3c6
theburningmonk <theburningmonk@gmail.com>
2017-12-24 00:03:28 +0000

    - removed awscred from init step into the get-index func

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const co       = require('co');
-const Promise  = require('bluebird');
-const awscred  = Promise.promisifyAll(require('awscred'));
+const co      = require('co');
+const Promise = require('bluebird');
 
 let initialized = false;
 
@@ -12,18 +11,13 @@ let init = co.wrap(function* () {
   }
 
   process.env.restaurants_api = "https://i3c5h755j0.execute-api.us-east-1.amazonaws.com/dev/restaurants";
-  process.env.restaurants_table        = "restaurants";
-  process.env.AWS_REGION               = "us-east-1";
-  process.env.cognito_user_pool_id     = "us-east-1_DfuAwa0vB";
-  process.env.cognito_client_id        = "test_cognito_client_id";
+  process.env.restaurants_table = "restaurants";
+  process.env.AWS_REGION = "us-east-1";
+  process.env.cognito_client_id = "test_cognito_client_id";
+  process.env.cognito_user_pool_id = "us-east-1_DfuAwa0vB";
   process.env.cognito_server_client_id = "niv7esuaibla0tj5q36b6mvnr";
-  
-  let cred = yield awscred.loadAsync();
-  
-  process.env.AWS_ACCESS_KEY_ID     = cred.credentials.accessKeyId;
-  process.env.AWS_SECRET_ACCESS_KEY = cred.credentials.secretAccessKey;
 
-  console.log('AWS credential loaded');
+  console.log("AWS credentials loaded");
 
   initialized = true;
 });
