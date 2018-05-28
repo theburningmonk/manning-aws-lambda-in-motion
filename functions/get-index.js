529bb08c7ed152b326ce22f4be674c2441029b1e
theburningmonk <theburningmonk@gmail.com>
2018-05-28 12:35:03 +0100

    - fixed failing tests due to X-ray

--- functions/get-index.js
+++ functions/get-index.js
@@ -49,16 +49,22 @@ function* getRestaurants() {
   
   return new Promise((resolve, reject) => {
     let f = co.wrap(function* (subsegment) {
-      subsegment.addMetadata('url', restaurantsApiRoot);
+      if (subsegment) {
+        subsegment.addMetadata('url', restaurantsApiRoot);  
+      }
 
       try {
         let body = (yield httpReq).body;
-        subsegment.close();
+        if (subsegment) {
+          subsegment.close();
+        }
         resolve(body);
       } catch (err) {
-        subsegment.close(err);
+        if (subsegment) {
+          subsegment.close(err);
+        }
         reject(err);
-      }      
+      }
     });
 
     // the current sub/segment
--- tests/steps/init.js
+++ tests/steps/init.js
@@ -11,12 +11,13 @@ let init = co.wrap(function* () {
     return;
   }
 
-  process.env.restaurants_api = "https://i3c5h755j0.execute-api.us-east-1.amazonaws.com/dev/restaurants";
+  process.env.restaurants_api = "https://8kbasri6v6.execute-api.us-east-1.amazonaws.com/dev/restaurants";
   process.env.restaurants_table = "restaurants";
   process.env.AWS_REGION = "us-east-1";
   process.env.cognito_client_id = "test_cognito_client_id";
   process.env.cognito_user_pool_id = "us-east-1_DfuAwa0vB";
   process.env.cognito_server_client_id = "niv7esuaibla0tj5q36b6mvnr";
+  process.env.AWS_XRAY_CONTEXT_MISSING = "LOG_ERROR";
 
   yield aws4.init();
 
